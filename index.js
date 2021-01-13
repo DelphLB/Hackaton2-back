b7e4bccbc188c2d47635149b18d8146ab30b76e4
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-01-13 17:07:13 +0100

    back ready

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
-const routes = require("./routes");
+const routes = require("./routes ");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api", routes);
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     throw err;
   }
--- routes /index.js
+++ routes /index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const playlists = require("./playlists");
-const tracks = require("./tracks");
+const playlists = require("./users");
+const tracks = require("./recettes");
 
-router.use("/playlists", playlists);
-router.use("/tracks", tracks);
+router.use("/users", playlists);
+router.use("/recettes", tracks);
 router.get("/", (req, res) => {
   res.send("OK");
 });
