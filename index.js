cfec022b44afa7ef10f0df1304f3d8db788060cd
RomainMouly <romainmouly@gmail.com>
2021-01-13 17:41:42 +0100

    twilio install

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const routes = require("./routes ");
+const pino = require("express-pino-logger")();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(pino);
 
 app.use("/api", routes);
 
